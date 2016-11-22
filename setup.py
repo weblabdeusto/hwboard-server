e78dc3da5aac4c35130fe21507e2cd6f1d919249
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2016-11-22 16:03:35 +0100

    Moved tests directory to a new folder; setup script now relies on find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='hwbserver',
       version='1.0',
@@ -8,5 +9,5 @@ setup(name='hwbserver',
       author='WebLab-Deusto',
       author_email='luis.rodriguezgil@deusto.es',
       url='https://github.com/weblabdeusto/hwbserver.git',
-      packages=['hwbserver'],
+      packages=find_packages()
      )
