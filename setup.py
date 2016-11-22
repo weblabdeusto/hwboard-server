de94fde3a395127e5052672cf09a7f4d0d86729e
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2016-11-22 10:42:32 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hwbserver',
+      version='1.0',
+      description='HardWareBoard Server (New FPGA)',
+      author='WebLab-Deusto',
+      author_email='luis.rodriguezgil@deusto.es',
+      url='https://github.com/weblabdeusto/hwbserver.git',
+      packages=['hwbserver'],
+     )
